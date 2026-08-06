71655476fc08be7b6a005bd30ce235edac5e6588
Aicha ELOUADI <e-aicha@live.fr>
2026-08-06 14:48:48 +0200

    feat: secure network access for Key Vault and Storage

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name       = "aelouadiRG"
-    storage_account_name      = "staelouaditfstate"
-    container_name            = "tfstate"
-    key                       = "bilan-azure-quiz.tfstate"
-    use_azuread_auth          = true
-    virtual_network_subnet_id = module.network.backend_subnet_id
+    resource_group_name  = "aelouadiRG"
+    storage_account_name = "staelouaditfstate"
+    container_name       = "tfstate"
+    key                  = "bilan-azure-quiz.tfstate"
+    use_azuread_auth     = true
   }
 }
